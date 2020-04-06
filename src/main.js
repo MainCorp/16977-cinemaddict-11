a951f6165e4c1c3c4bb1a56eb5df3d79797d11c5
MainCorp <maincorp228@gmail.com>
2020-04-06 06:32:02 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -68,28 +68,6 @@
   const templateCustomCardTopRated = () => {
     return (`
       <article class="film-card">
-        <h3 class="film-card__title">The Dance of Life</h3>
-        <p class="film-card__rating">8.3</p>
-        <p class="film-card__info">
-          <span class="film-card__year">1929</span>
-          <span class="film-card__duration">1h 55m</span>
-          <span class="film-card__genre">Musical</span>
-        </p>
-        <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
-        <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
-        <a class="film-card__comments">5 comments</a>
-        <form class="film-card__controls">
-          <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-          <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-          <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
-        </form>
-      </article>
-    `);
-  };
-
-  const templateCustomCardMostCommented = () => {
-    return (`
-      <article class="film-card">
         <h3 class="film-card__title">The Man with the Golden Arm</h3>
         <p class="film-card__rating">9.0</p>
         <p class="film-card__info">
@@ -104,6 +82,28 @@
           <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
           <button class="film-card__controls-item button film-card__controls-item--mark-as-watched  film-card__controls-item--active">Mark as watched</button>
           <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+        </form>
+      </article>
+    `);
+  };
+
+  const templateCustomCardMostCommented = () => {
+    return (`
+      <article class="film-card">
+        <h3 class="film-card__title">Santa Claus Conquers the Martians</h3>
+        <p class="film-card__rating">2.3</p>
+        <p class="film-card__info">
+          <span class="film-card__year">1964</span>
+          <span class="film-card__duration">1h 21m</span>
+          <span class="film-card__genre">Comedy</span>
+        </p>
+        <img src="./images/posters/santa-claus-conquers-the-martians.jpg" alt="" class="film-card__poster">
+        <p class="film-card__description">The Martians Momar ("Mom Martian") and Kimar ("King Martian") are worried that their children Girmar ("Girl Martian") and Bomar ("Boy Marti…</p>
+        <a class="film-card__comments">465 comments</a>
+        <form class="film-card__controls">
+          <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
+          <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
+          <button class="film-card__controls-item button film-card__controls-item--favorite film-card__controls-item--active">Mark as favorite</button>
         </form>
       </article>
     `);
